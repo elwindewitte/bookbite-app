289a853e501953eae7696adba244120af217ed1a
Elwin <elwindewitte@gmail.com>
2020-12-14 17:04:11 +0100

    First compile

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+//This will ensure these folders are added to the _site folder once you compile with Eleventy
 module.exports = function (eleventyCofig) {
     eleventyCofig.addPassthroughCopy("images");
     eleventyCofig.addPassthroughCopy("admin");
