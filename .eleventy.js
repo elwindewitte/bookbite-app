28f53dff1e830f868901529278fc00ed6efabde1
Elwin <elwindewitte@gmail.com>
2020-12-15 18:06:36 +0100

    Started to add the styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 //This will ensure these folders are added to the _site folder once you compile with Eleventy
 module.exports = function (eleventyCofig) {
     eleventyCofig.addPassthroughCopy("assets");
+    eleventyCofig.addPassthroughCopy("css");
 };
