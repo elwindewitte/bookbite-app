7ce3e1dd732664a22091158428acb23c0de61631
Elwin <elwindewitte@gmail.com>
2020-12-23 14:23:48 +0100

    Authentication works

--- _site/js/authentication.js
+++ _site/js/authentication.js
@@ -1,8 +1,24 @@
-const buttonSignIn = document.getElementById("buttonSignIn");
-const inputUsername = document.getElementById("userUsername");
-const inputPassword = document.getElementById("userPassword");
+window.onload = function () {
+    // Defining variables
+    var buttonSignIn = document.getElementById("buttonSignIn");
+    var inputUsername = document.getElementById("userUsername");
+    var inputPassword = document.getElementById("userPassword");
+    var dataUsername = localStorage.getItem("username");
+    var dataPassword = localStorage.getItem("password");
+    var dataOnboardingDone = localStorage.getItem("onboardingDone");
+    var path = window.location.protocol + '//' + window.location.host + '/';
 
-buttonSignIn.onclick = function () {
-    console.log(inputUsername.value);
-    console.log(inputPassword.value);
+    // Actual authentication, or simulation of at least
+    buttonSignIn.onclick = function () {
+        if ((inputUsername.value == dataUsername) && (inputPassword.value == dataPassword)) {
+            localStorage.setItem("signinDone", "true");
+            if (dataOnboardingDone == 'true') {
+                // Redirect to home page when the onboarding is completed
+                window.location.replace(path + 'home/');
+            } else {
+                // Redirect to onboarding when it hasn't been completed yet
+                window.location.replace(path + 'onboarding/');
+            };
+        };
+    };
 };
---
+++ _site/js/data.js
@@ -0,0 +1,4 @@
+localStorage.setItem("username", "elwindewitte");
+localStorage.setItem("password", "Elwin0910!");
+// localStorage.setItem("signinDone", "false");
+// localStorage.setItem("onboardingDone", "true");
